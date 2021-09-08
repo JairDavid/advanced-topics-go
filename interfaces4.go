8ac26f1575c6acdbddf170c811076bea6983ef8a
JairDavid <jairvasmar@gmail.com>
2021-09-08 13:02:36 -0500

    some files added

---
+++ interfaces4.go
@@ -0,0 +1,44 @@
+package main
+
+import "fmt"
+
+type Methods interface {
+	connect(bool)
+	status()
+}
+
+type Deltails struct {
+	name   string
+	model  string
+	madeIn string
+}
+
+type Television struct {
+	Deltails
+	on      bool
+	channel int
+	volume  int
+}
+
+type Radio struct {
+	Deltails
+	on        bool
+	frecuency float64
+	volume    int
+}
+
+type Laptop struct {
+	Deltails
+	on       bool
+	internet bool
+}
+
+func (d *Deltails) showDeltails() {
+	fmt.Println("Name: ", d.name)
+	fmt.Println("Model: ", d.model)
+	fmt.Println("Maded in: ", d.madeIn)
+}
+
+func main() {
+	fmt.Println("xd")
+}
